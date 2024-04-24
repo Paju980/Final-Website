759d652aa82bba8608d5f32816f6d77c83a6635e
Paju980 <prajwalwandhare100@gmail.com>
2024-04-24 10:23:56 +0530

    added images

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './AppDownload.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 const AppDownload = () => {
   return (
-    <div className='app-download' id='app-download'>
-        <p>For Better Experience Doenload <br /> App</p>
-        <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" srcset="" />
-            <img src={assets.app_store} alt="" srcset="" />
-        </div>
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> App
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" srcset="" />
+        <img src={assets.app_store} alt="" srcset="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AppDownload
+export default AppDownload;
